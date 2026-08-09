310087a84c31e8d444afd3a16cdfdb12d4fd0232
Earl Patrick Bellinger <earlbellinger@gmail.com>
2026-08-08 20:02:21 -1000

    Prevent equations panel overflow

--- python_implementation/web_app/dist/assets/wizard_of_oz-file.js
+++ python_implementation/web_app/dist/assets/wizard_of_oz-file.js
@@ -42770,7 +42770,7 @@ ${rows.map((row) => headers.map((header) => csvCell(row[header])).join(",")).joi
       application: {
         name: "OZwizard",
         version: "1.0.0",
-        sourceCommit: "5d33566",
+        sourceCommit: "fde3f27",
         sourceUrl: window.location.href.split("#")[0]
       },
       createdAt: (/* @__PURE__ */ new Date()).toISOString(),
